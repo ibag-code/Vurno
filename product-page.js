08f967b003c62078bc052fa9df93e0c05e907acc
ibag <ibagdesignengineer@gmail.com>
2026-08-25 15:01:25 +0100

    adding id to each data implementing module to prevent naming conflict

--- product-page.js
+++ product-page.js
@@ -1,78 +1,8 @@
-const productInfo = [{
-    image:  "images/product/athletic-cotton-socks-6-pairs.jpg",
-    productName: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-        stars: '4.5',
-        ratingNumber: 87
-    },
-    pricing: "699",
-},
-{
-    image:  "images/product/Basketball.jpg ",
-    productName: "Intermediate Size Basketball",
-    rating: {
-        stars: '5.0',
-        ratingNumber: 40
-    },
-    pricing: "1299",
-},
-{
-    image:  "images/product/shirt.jpg",
-    productName: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-        stars: '4.0',
-        ratingNumber: 87
-    },
-    pricing: "6599",
-},
-{
-    image:  "images/product/black-2-slot-toaster.jpg",
-    productName: "2 Slot Toaster - Black",
-    rating: {
-        stars:'4.5',
-        ratingNumber: 70
-    },
-    pricing: "2399",
-},
-{
-    image:  "images/product/6-piece-white-dinner-plate-set.jpg",
-    productName: "6 Piece White Dinner Plate Set",
-    rating: {
-        stars: '4.5',
-        ratingNumber: 87
-    },
-    pricing: "1099",
-},
-{
-    image:  "images/product/6-piece-non-stick-baking-set.webp",
-    productName: "6-Piece Nonstick, Carbon Steel Oven Bakeware ",
-    rating: {
-        stars: '4.5',
-        ratingNumber: 87
-    },
-    pricing: "699",
-},
-{
-    image:  "images/product/plain-hooded-fleece-sweatshirt-yellow.jpg",
-    productName: "Plain Hooded Fleece Sweatshirt",
-    rating: {
-        stars: '5.0',
-        ratingNumber: 56
-    },
-    pricing: "9899",
-},
-{
-    image:  "images/product/luxury-tower-set-6-piece.jpg",
-    productName: "Luxury Towel Set - Graphite Gray",
-    rating: {
-        stars: '5.0',
-        ratingNumber: 120
-    },
-    pricing: "1099", 
-},
-]
+import {cart} from './cart-page.js';
+import {productInfo} from './product-info.js'
 
-//COMBINING ALL HTML 
+
+//COMBINING ALL HTML  Or Viewer
 let productHTMl = '';
 
 productInfo.forEach((product) => {
@@ -126,7 +56,7 @@ productInfo.forEach((product) => {
                 </div>
 
             </div>
-                <button class="call-to-action js-cta" data-products-name ="${product.productName}"> 
+                <button class="call-to-action js-cta" data-products-name ="${product.id}"> 
                 Add to cart</button>
         </div>`
 
@@ -141,26 +71,19 @@ document.querySelector('.js-product-grid').innerHTML = productHTMl;
 
 
 
-//This give us the list of NodeList all the Add to cart buttom
-//On the page
+
+// The call to action button Or Controller
 let ctaBtn = document.querySelectorAll('.js-cta');
 
 
-//Looping through each of the button
-//querySelectorAll returns a NodeList — which is not a single 
-// element, it's a collection of elements. 
-// Think of it like an array of all the buttons on the page
 
-
-///"Loop through every ctaBtn I selected, 
-// and for each one, call it button temporarily"
 ctaBtn.forEach((button) => {
 
     //"When this specific button is clicked, run the following code"    
     button.addEventListener('click', function() {
 
         //dataset gives us all the data attached to the CTA button
-        const dataproductName = button.dataset.productsName;
+        const dataproductId = button.dataset.productsId;
 
  
         const productCard = button.closest('.product-image-content');
@@ -171,30 +94,20 @@ ctaBtn.forEach((button) => {
         console.log(dropdown)
 
 
-
-
-        /*
-        const productCard = button.closest('.product-image-content');
-        const dropdown = document.querySelector(`.js-quantity-selector-${dataproductName.replace(/\s/g, '')}`);
-        let selectedItem = Number(dropdown.value);
-        const addText = productCard.querySelector('.added-to-cart');
-        */
-
-
         addText.classList.add('visible');
 
         setTimeout(function() {
             addText.classList.remove('visible')
         }, 1000)
-
+ 
 
 
 
         let itemMatching;
 
-        cart.forEach(function(item) {
+        cart.forEach((item) => {
             //checking if a product is already in a cart
-            if(dataproductName === item.productName){
+            if(dataproductId === item.productsId){
                 itemMatching = item;
             }   
         });
@@ -204,51 +117,22 @@ ctaBtn.forEach((button) => {
 
                 //if it is not in the cart add to the cart
             } else {
-                //After getting the name of the produuct when clicking ADD to cart
-                //them push the name and other data to the cart page.
+
                 cart.push({
-                    productName: dataproductName,
+                    productId: dataproductId,
                     quantity: selectedItem,
                 })
             }
-            console.log (cart)
-
 
             //calculating the total Quantity
-            let quantityCart = 0;//Adding up al the quantity nd save it here
+            let quantityCart = 0;
 
-            cart.forEach(function(item) {
+            cart.forEach((item) => {
                 quantityCart += item.quantity;
             })
 
             document.querySelector('.js-cart-number').innerHTML = quantityCart;
-
-            console.log (quantityCart);
-
-            
-
-            
     })
-})
-
-
-
-const inputFieldBtn = document.querySelector('.input-field');
-const iconBtn = document.querySelector('.search-bar-icon');
-
-iconBtn.addEventListener('click', function () {
-    inputFieldBtn.classList.toggle('open')
-
-    if (inputFieldBtn.classList.contains('open')) {
-        inputFieldBtn.focus();
-    }
-        
-})
-
-
-
-
-
-
+});
 
 
